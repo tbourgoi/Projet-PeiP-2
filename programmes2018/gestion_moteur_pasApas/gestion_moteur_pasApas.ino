56a333b02634b476635f62f7a456f92c86859fd0
Fincha0 <45673674+Fincha0@users.noreply.github.com>
2019-03-01 15:15:27 +0100

    mise à jour gestion gestion moteur + pièces V2

--- programmes2018/gestion_moteur_pasApas/gestion_moteur_pasApas.ino
+++ programmes2018/gestion_moteur_pasApas/gestion_moteur_pasApas.ino
@@ -3,26 +3,36 @@
 
 Servo leve;
 
-int borneMin = 25;
-int borneMax = 100;
+int pointHaut = 25;
+int pointBas = 100;
 int position;
+int latence = 10;
+
 void setup() {
     Serial.begin(9660);
     Serial.print("initialisé");
     leve.attach(10);
-    leve.write(borneMin);
+    leve.write(pointHaut);
+	  pinMode(2, INPUT);
+	
 }
 
 void loop() {
- /*
-  for (int position = borneMin ; position <= borneMax; position++) {
+ 
+  for (int position = pointHaut ; position <= pointBas && digitalRead(2) == LOW ; position++) {
     leve.write(position);
-    delay(25);
+    delay(latence);
   }
   
   // Fait bouger le bras de 180° à 10°
-  for (int position = borneMax; position >= borneMin; position--) {
+  for (int position = pointBas; position >= pointHaut && digitalRead(2) == LOW; position--) {
     leve.write(position);
-    delay(25);
-  }*/
+    delay(latence);
+  }
+
+  if (digitalRead(2) == HIGH){
+	  position = pointBas;
+	  leve.write(pointBas);
+  }
+
 }
